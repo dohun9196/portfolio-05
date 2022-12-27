cdeab8f501a323ed6c3f0da243f2db9098c3c0ab
Dohun9196 <68097610+dohun9196@users.noreply.github.com>
2022-12-27 17:46:01 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -27,53 +27,38 @@ $(function () {
         fade: true,
         cssEase: 'linear',
     });
+
+
+    $('.down_con a').click(function () {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
+    });
+
+    $('.kawasaki_group ul>li a').click(function () {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
+    });
+
+    $(window).scroll(function () {
+        var scrollValue = $(document).scrollTop();
+        console.log(scrollValue);
+    });
+
+    $(window).scroll(function (c) {
+        var height = $(document).scrollTop();
+
+        if (height > 1967) {
+            $('.slideTitle').addClass('fix');
+        } else if (height < 1967) {
+            $('.slideTitle').removeClass('fix');
+        }
+    });
 });
 
-//     $('.trip_slide').slick({
-//         infinite: true,
-//         slidesToShow: 4,
-//         slidesToScroll: 1,
 
-//         prevArrow: $('#Trip_prev'),
-//         nextArrow: $('#Trip_next'),
-//     });
-
-//     var slickNum = $('.mainBtn .num span');
-//     $('.main_slide').on('init reInit afterChange', function (e, s, c) {
-
-//         var i = (c ? c : 0) + 1;
-//         slickNum.text(i + 0);
-//     });
-
-
-
-//     $('.banner_bottom a').on('click', function () {
-//         var banner = $('.bannerWrap');
-//         $(this).toggleClass('on')
-
-//         if ($(this).hasClass('on')) {
-//             $(banner).removeClass('on');
-//         } else {
-//             $(banner).addClass('on');
-//         }
-//     });
-
-
-//     $('.banner_tit span').on('click', function () {
-//         $('.banner_tit span').not($(this)).removeClass('on');
-//         $(this).toggleClass('on');
-
-//     });
-
-
-
-//     $('.bmain_header span').on('click', function () {
-//         $('.bmain_header span').not($(this)).removeClass('on');
-//         $(this).toggleClass('on');
-//     });
-//     $('.banner_seat span').on('click', function () {
-//         $('.banner_seat span').not($(this)).removeClass('on');
-//         $(this).toggleClass('on');
-//     });
 
 
