be2664a952fd2a78f84fbe1f5452f2d663afa2fc
Dohun9196 <68097610+dohun9196@users.noreply.github.com>
2022-12-28 17:50:25 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
 $(function () {
 
-    $('.mainVisual .main_slide').slick({
-        dots: false,
-        autoplay: true,
-        arrows: false,
+    // $('.mainVisual .main_slide').slick({
+    //     dots: false,
+    //     autoplay: true,
+    //     arrows: false,
 
 
-        autoplaySpeed: 4000,
+    //     autoplaySpeed: 4000,
 
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
+    //     infinite: true,
+    //     speed: 500,
+    //     fade: true,
+    //     cssEase: 'linear',
+    // });
+
+    $('#background').YTPlayer({
+        mute: true,
+        showControls: false,
+        useOnMobile: true,
+        quality: 'highres',
+        containment: '#background',
+        loop: true,
+        autoPlay: true,
+        stopMovieOnBlur: false,
+        startAt: 0,
+        opacity: 1,
     });
 
     $('.kawasakiSlide .main_slide').slick({
@@ -57,6 +70,20 @@ $(function () {
             $('.slideTitle').removeClass('fix');
         }
     });
+
+
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true,
+        'alwaysShowNavOnTouchDevices': true,
+        // 'disableScrolling': true,  
+        // { 이게 있으면 over, close 되었을 때 x축으로 스크롤바 생김 }
+        'alwaysShowClose': true,
+        'maxWidth': 1200,
+        'maxHeight': 800,
+        'fitImagesInViewport': true,
+
+    })
 });
 
 
